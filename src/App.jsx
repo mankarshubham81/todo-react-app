ed45958f661af49a1c04333253c075ba517c3ee5
Shubham Mankar <mankarshubham81@gmail.com>
2024-03-27 11:28:24 +0530

    added UI xomponents of todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
-
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import { useContext, useEffect } from "react";
+import axios from "axios";
+import { Context, server } from "./main";
 
 function App() {
-  return <Router>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
+  const { setUser, setIsAuthenticated, setLoading } = useContext(Context);
 
-    </Routes>
-  </Router>
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`https://todoapi-lwhd.onrender.com`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUser(res.data.user);
+        setIsAuthenticated(true);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setUser({});
+        setIsAuthenticated(false);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/app.scss";
+import { createContext } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const server = "https://todoapi-lwhd.onrender.com/api/v1";
+
+export const Context = createContext({ isAuthenticated: false });
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  return (
+    <Context.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        loading,
+        setLoading,
+        user,
+        setUser,
+      }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppWrapper />
+  </React.StrictMode>
+);
